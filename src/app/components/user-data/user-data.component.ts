f8fa39c20d1efa51eca2595fb773ae9731513a15
Sapna Sisodiya <sapna.sisodiya@tatvasoft.com>
2023-10-30 16:28:33 +0530

    complete webapi

--- src/app/components/user-data/user-data.component.ts
+++ src/app/components/user-data/user-data.component.ts
@@ -11,8 +11,10 @@ import { ToastService } from 'src/app/services/toast.service';
 })
 export class UserDataComponent {
   employees: User[] = [];
+  employees1: User[] = [];
   userAdded = false;
   addError = false;
+  _listFilter = '';
 
   constructor(
     private employeesservice: UserService,
@@ -23,12 +25,35 @@ export class UserDataComponent {
   ngOnInit(): void {
     this.getAllUserList();
   }
+  get listFilter(): string {
+    return this._listFilter;
+  }
+// Inside your UserDataComponent class
+set listFilter(value: string) {
+  this._listFilter = value;
+  // Check if the search input is empty, and if it is, show all data
+  if (!this.listFilter) {
+   this.getAllUserList();
+  } else {
+    // Filter the data based on the search input
+    this.employees = this.performFilter(this.listFilter);
+  }
+}
+
+
+  performFilter(filterBy: string): User[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.employees.filter(
+      (employee: User) =>
+        employee.firstName &&
+        employee.firstName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   getAllUserList() {
     this.employeesservice.getAllUser().subscribe({
       next: (employees) => {
         this.employees = employees;
-        
       },
       error: (respose) => {
         console.log(respose);
--- src/app/components/user-data/user-data.module.ts
+++ src/app/components/user-data/user-data.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { UserDataRoutingModule } from './user-data-routing.module';
 import { UserDataComponent } from './user-data.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
-
 @NgModule({
   declarations: [UserDataComponent, UserDetailsComponent],
-  imports: [CommonModule, UserDataRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    UserDataRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class UserDataModule {}
